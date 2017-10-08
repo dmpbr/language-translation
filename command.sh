e403263f6a9d1a64daa48c90fc0ce79af660c3ca
diogo <test.dribeiro@gmail.com>
2017-10-07 19:02:09 -0700

    Remove file

--- command.sh
+++
@@ -1,3 +0,0 @@
-cp -r /code/* /output
-cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/gwVveCV7EVDHhADQAh3ngA' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.tornado_settings="{'headers': {'Content-Security-Policy': \"frame-ancestors 'self' www.floydhub.com \"}}" --NotebookApp.iopub_data_rate_limit=1.0e10
